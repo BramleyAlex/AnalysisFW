e2eb3f878b6b72890bda8ac7444c551b4b2c5800
BramleyAlex <alexander.bennett-5@student.manchester.ac.uk>
2024-08-21 16:47:10 +0100

    Added BDT variables

--- include/VBFBDT.h
+++ include/VBFBDT.h
@@ -11,12 +11,9 @@ class VBFBDT {
         m_weightsFilePath = weightsFilePath.c_str();
         m_reader = std::make_unique<TMVA::Reader>("Silent");
         m_reader->AddVariable("mjj",&m_bdt_mjj);
-        m_reader->AddVariable("deltaRapidity",&m_bdt_drap);
-        m_reader->AddVariable("ptBalance",&m_bdt_ptbal);
-        m_reader->AddVariable("zCentrality",&m_bdt_zcen);
-        m_reader->AddVariable("omega",&m_bdt_omega);
-        //reader->AddVariable("transverseMassLep",&bdt_transmasslep);
-        m_reader->AddVariable("transverseRecoMassVariable",&m_bdt_transmasslep); // For transverse-reco mass ratio
+        m_reader->AddVariable("JetGap",&m_bdt_drap);
+        m_reader->AddVariable("PtBalance",&m_bdt_ptbal);
+        m_reader->AddVariable("centrality",&m_bdt_zcen);
         m_reader->AddSpectator("eventNumber", &m_bdt_eventNumber); // For deterministic split
         m_reader->BookMVA("VBF_BDT", weightsFilePath.c_str());
     }
@@ -30,32 +27,25 @@ class VBFBDT {
             m_bdt_drap = other.m_bdt_drap;
             m_bdt_ptbal = other.m_bdt_ptbal;
             m_bdt_zcen = other.m_bdt_zcen;
-            m_bdt_omega = other.m_bdt_omega;
-            m_bdt_transmasslep = other.m_bdt_transmasslep;
             m_bdt_eventNumber = other.m_bdt_eventNumber;
             m_weightsFilePath = other.m_weightsFilePath;
 
             m_reader->AddVariable("mjj",&m_bdt_mjj);
-            m_reader->AddVariable("deltaRapidity",&m_bdt_drap);
-            m_reader->AddVariable("ptBalance",&m_bdt_ptbal);
-            m_reader->AddVariable("zCentrality",&m_bdt_zcen);
-            m_reader->AddVariable("omega",&m_bdt_omega);
-            //reader->AddVariable("transverseMassLep",&bdt_transmasslep);
-            m_reader->AddVariable("transverseRecoMassVariable",&m_bdt_transmasslep); // For transverse-reco mass ratio
+            m_reader->AddVariable("JetGap",&m_bdt_drap);
+            m_reader->AddVariable("PtBalance",&m_bdt_ptbal);
+            m_reader->AddVariable("centrality",&m_bdt_zcen);
             m_reader->AddSpectator("eventNumber", &m_bdt_eventNumber); // For deterministic split
             m_reader->BookMVA("VBF_BDT",  other.m_weightsFilePath);
         }
         return *this;
     }
 
-    void update(float mjj, float drap, float ptbal, float zcen, float omega, float transmasslep, float eventNumber) {
+    void update(float mjj, float drap, float ptbal, float zcen, long eventNumber) {
         m_bdt_mjj = mjj;
         m_bdt_drap = drap;
         m_bdt_ptbal = ptbal;
         m_bdt_zcen = zcen;
-        m_bdt_omega = omega;
-        m_bdt_transmasslep = transmasslep;
-        m_bdt_eventNumber = eventNumber;
+        m_bdt_eventNumber = eventNumber % 10;
     }
 
     double evaluate() {
@@ -69,8 +59,6 @@ class VBFBDT {
         m_bdt_drap = 0;
         m_bdt_ptbal = 0;
         m_bdt_zcen = 0;
-        m_bdt_omega = 0;
-        m_bdt_transmasslep = 0;
         m_bdt_eventNumber = 0;
     }
 
@@ -80,9 +68,6 @@ class VBFBDT {
     float m_bdt_drap;
     float m_bdt_ptbal;
     float m_bdt_zcen;
-    float m_bdt_omega;
-    float m_bdt_transmasslep;
-    float m_bdt_eventNumber;
+    int m_bdt_eventNumber;
     const char* m_weightsFilePath;
-
 };
